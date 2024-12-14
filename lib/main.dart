0698fc6652e330c8499f8c1149a24f5cf6de0708
John Reg <johnreginaldjao@gmail.com>
2024-12-14 22:37:08 +0800

    handled if user logged in or verified

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app_flutter/firebase_options.dart';
 import 'package:todo_app_flutter/views/login_view.dart';
 import 'package:todo_app_flutter/views/register_view.dart';
+import 'package:todo_app_flutter/views/verify_email_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -40,13 +42,22 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                return const Text("Email is verified");
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
           default:
             return Scaffold(
               appBar: AppBar(
                 title: const Text("Loading"),
               ),
-              body: const CircularProgressIndicator(),
+              body: const Center(child: CircularProgressIndicator()),
             );
         }
       },
